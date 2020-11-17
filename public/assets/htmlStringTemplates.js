24c54a3eb740c862d8abb836b672a3fbe127c789
Benjo <beniamino.tartarini@gmail.com>
2020-11-17 17:49:06 +0900

    change newsletter footer color

--- public/assets/htmlStringTemplates.js
+++ public/assets/htmlStringTemplates.js
@@ -137,8 +137,8 @@ table[class="body"] .article {
 						<div class="footer" style="clear: both; margin-top: 10px; text-align: center; width: 100%;">
 							<table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
 								<tr>
-									<td class="content-block" style="font-family: 'Courier New', sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
-										<span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">www.lasfrocchia.com</span>
+									<td class="content-block" style="font-family: 'Courier New', sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #1976d2; font-size: 14px; text-align: center;" valign="top" align="center">
+										<span class="apple-link" style="color: #1976d2; font-size: 14px; text-align: center;">www.lasfrocchia.com</span>
 									</td>
 								</tr>
 							</table>
